f41b0d4c7822dfa18b4ef39678c29dac2cb55645
John Doe <johndoe@example.com>
2023-05-31 15:44:31 +0300

    fix

--- internal/service/tg_service/channel_post.go
+++ internal/service/tg_service/channel_post.go
@@ -1,8 +1,10 @@
 package tg_service
 
 import (
+	"errors"
 	"fmt"
 	"myapp/internal/models"
+	"myapp/internal/repository"
 	"time"
 )
 
@@ -16,9 +18,17 @@ func (srv *TgService) Donor_addChannelPost(m models.Update) error {
 	message_id := m.ChannelPost.MessageId
 	channel_id := m.ChannelPost.Chat.Id
 
+	post, err := srv.As.GetPostByDonorIdAndChId(message_id, channel_id)
+	if err != nil && !errors.Is(err, repository.ErrNotFound) {
+		return err
+	}
+	if post.PostId != 0 {
+		srv.l.Info("пост уже есть в БД, валим!")
+		return nil
+	}
 
 	// добавили пост в БД
-	err := srv.As.AddNewPost(channel_id, message_id, message_id)
+	err = srv.As.AddNewPost(channel_id, message_id, message_id)
 	if err != nil {
 		return err
 	}
