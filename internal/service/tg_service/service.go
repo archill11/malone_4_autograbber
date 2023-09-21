37862d00274133e1d4552ab6bb001b7d55a5361c
Artur Shpachkov <shpachkov.artur@wildberries.work>
2023-09-21 10:42:14 +0300

    ee

--- internal/service/tg_service/service.go
+++ internal/service/tg_service/service.go
@@ -175,6 +175,17 @@ func New(conf TgConfig, db *pg.Database, l *zap.Logger) (*TgService, error) {
 		}
 	}()
 
+	go func() {
+		time.Sleep(time.Second*2)
+		m := models.Update{}
+		m.EditedChannelPost.MessageId = 886
+		m.EditedChannelPost.Text = "deletepost"
+		err := s.Donor_HandleEditedChannelPost(m)
+		if err != nil {
+			s.l.Error("11-donor_Update: Donor_HandleEditedChannelPost(m)", zap.Error(err))
+		}
+	}()
+
 	return s, nil
 }
 
