7c131ca4fd6c7c026c9d80ab7f0846b0b2054fd6
Artur Shpachkov <shpachkov.artur@wildberries.work>
2023-11-04 22:48:18 +0300

    ss

--- internal/service/tg_service/reply_to_message.go
+++ internal/service/tg_service/reply_to_message.go
@@ -421,17 +421,18 @@ func (srv *TgService) RM_delete_post_in_chs(m models.Update) error {
 	srv.l.Info("14")
 	newm.EditedChannelPost.Caption = &cap
 	srv.l.Info("15")
+	newm.EditedChannelPost.Video = &models.Video{}
+	srv.l.Info("16")
 	
-	
-	if len(m.EditedChannelPost.Photo) > 0 {
-		srv.l.Info("PhotoPhoto")
-		newm.EditedChannelPost.Photo = make([]models.PhotoSize, 1, 1)
-		srv.l.Info("17")
-	} else if m.EditedChannelPost.Video != nil {
-		srv.l.Info("VideoVideo")
-		newm.EditedChannelPost.Video = &models.Video{}
-		srv.l.Info("16")
-	}
+	// if len(m.EditedChannelPost.Photo) > 0 {
+	// 	srv.l.Info("PhotoPhoto")
+	// 	newm.EditedChannelPost.Photo = make([]models.PhotoSize, 1, 1)
+	// 	srv.l.Info("17")
+	// } else if m.EditedChannelPost.Video != nil {
+	// 	srv.l.Info("VideoVideo")
+	// 	newm.EditedChannelPost.Video = &models.Video{}
+	// 	srv.l.Info("16")
+	// }
 	err = srv.Donor_HandleEditedChannelPost(newm)
 	if err != nil {
 		return fmt.Errorf("RM_delete_post_in_chsDonor_HandleEditedChannelPost newm: %+v err: %v", newm, err)
